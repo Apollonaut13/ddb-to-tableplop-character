ceb2616e7229ea4984843696f977e3543e080bdd
Apollonaut13 <54721145+Apollonaut13@users.noreply.github.com>
2022-01-15 13:45:59 -0800

    Add files via upload

---
+++ ddbCharacter_to_tableplop.py
@@ -0,0 +1,389 @@
+import requests
+import json
+import os
+
+
+def main():
+    characterURL = input("Paste the URL for your character below.\n> ")
+    characterID = characterURL.split('/')[-1]
+    if 'ddb.ac' in characterURL:
+        characterID = characterURL.split('/')[-2]
+
+    try:
+        r = requests.get(f"https://character-service.dndbeyond.com/character/v3/character/{characterID}")
+        characterData = json.loads(r.text)
+
+        if characterData['data'].get('name'):
+            print(f"Loaded {characterData['data']['name']}'s character data.")
+        else:
+            print("Character not found. Exiting.")
+            exit()
+    except Exception as e:
+        print(e)
+
+    with open(f"{characterData['data']['name']}.json", 'w') as outputFile:
+        json.dump(characterData, outputFile, indent=4)
+    characterData = characterData['data']
+
+    dnd_skills = {
+        'athletics': 'strength',
+        'acrobatics': 'dexterity',
+        'sleight-of-hand': 'dexterity',
+        'stealth': 'dexterity',
+        'arcana': 'intelligence',
+        'history': 'intelligence',
+        'investigation': 'intelligence',
+        'nature': 'intelligence',
+        'religion': 'intelligence',
+        'animal-handling': 'wisdom',
+        'insight': 'wisdom',
+        'medicine': 'wisdom',
+        'perception': 'wisdom',
+        'survival': 'wisdom',
+        'deception': 'charisma',
+        'intimidation': 'charisma',
+        'performance': 'charisma',
+        'persuasion': 'charisma'
+    }
+
+    dnd_stats = ['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma']
+
+
+    class Character:
+        def __init__(self):
+            self.name = ""
+            self.level = 0
+            self.classNames = ""
+            self.castingClass = ""
+            self.armorClass = 0
+            self.maxHP = 0
+
+            self.stats = {}
+            self.skills = {}
+            self.saves = {}
+
+        @property
+        def stat_modifier(self):
+            return {key: (value-10) // 2 for key, value in self.stats.items()}
+
+
+    c = Character()
+    c.name = characterData.get('name')
+    c.level = sum(eachClass.get('level') for eachClass in characterData.get('classes'))
+
+    classComboString = ""
+    for i, eachClass in enumerate(characterData.get('classes')):
+        className = eachClass['definition']['name']
+        subclass = None
+        try:
+            subclass = eachClass.get('subclassDefinition').get('name')
+        except AttributeError:
+            pass
+        if subclass:
+            className += f' ({subclass})'
+        className += f" {eachClass.get('level')}"
+        classComboString += className
+        if i < len(characterData.get('classes')) - 1:
+            classComboString += '/'
+    c.classNames = classComboString
+
+    baseStats = [stat['value'] for stat in characterData.get('stats')]
+    bonusStats = [stat['value'] for stat in characterData.get('bonusStats')]
+    overrideStats = [stat['value'] for stat in characterData.get('overrideStats')]
+
+    for i in range(6):
+        if bonusStats[i]:
+            baseStats[i] += bonusStats[i]
+        if overrideStats[i]:
+            baseStats[i] = overrideStats[i]
+
+    for i, statName in enumerate(dnd_stats):
+        c.stats[statName] = baseStats[i]
+
+
+    misc_ac_bonus = 0
+    for race_Class_Background_Item_Feat_Condition, feature in characterData.get('modifiers').items():
+        for detail in feature:
+            if detail.get('type') == 'proficiency':
+                if detail.get('subType') in dnd_skills.keys():
+                    c.skills[detail.get('subType')] = 1
+                if detail.get('subType').endswith('saving-throws'):
+                    c.saves[detail.get('subType').replace('-saving-throws', '')] = 1
+            if detail.get('type') == 'expertise':
+                if detail.get('subType') in dnd_skills.keys():
+                    c.skills[detail.get('subType')] = 2
+            if detail.get('type') == 'bonus':
+                if detail.get('subType').endswith('score') and not detail.get('subType').startswith('choose'):
+                    c.stats[detail.get('subType').split('-')[0]] += detail.get('value')
+                if detail.get('subType').endswith('armor-class'):
+                    misc_ac_bonus += detail.get('value')
+
+    armorEquipped = False
+    for item in characterData.get('inventory'):
+        if item['equipped'] and item['definition'].get('grantedModifiers'):
+            for grant in item['definition'].get('grantedModifiers'):
+                if grant['subType'] == 'armor-class':
+                    misc_ac_bonus += grant['value']
+
+        if item['equipped'] and item['definition'].get('armorTypeId'):
+            c.armorClass += item['definition'].get('armorClass')
+
+            match item['definition'].get('baseArmorName'):
+                case "Padded" | "Leather" | "Studded Leather":
+                    armorEquipped = True
+                    c.armorClass += c.stat_modifier['dexterity']
+                case "Hide" | "Chain Shirt" | "Scale Mail" | "Breastplate" | "Half Plate":
+                    armorEquipped = True
+                    c.armorClass += min(2, c.stat_modifier['dexterity'])
+                case "Ring Mail" | "Chain Mail" | "Splint" | "Plate":
+                    armorEquipped = True
+
+    # if a shield is equipped, c.armorClass already accounted for it
+    if not armorEquipped:
+        c.armorClass += 10 + c.stat_modifier['dexterity']
+        if 'Monk' in c.classNames:
+            c.armorClass = max(
+                c.stat_modifier['wisdom'] + c.stat_modifier['dexterity'],
+                c.armorClass)
+
+        if 'Barbarian' in c.classNames:
+            c.armorClass = max(
+                c.stat_modifier['constitution'] + c.stat_modifier['dexterity'],
+                c.armorClass)
+
+    c.armorClass += misc_ac_bonus
+
+    initiative = c.stat_modifier['dexterity']
+    if 'Ranger (Gloom Stalker)' in c.classNames:
+        initiative += c.stat_modifier['wisdom']
+    if 'Wizard (War)' in c.classNames:
+        initiative += c.stat_modifier['intelligence']
+
+    sheet = {
+        "stats": {},
+        "info": {},
+        "appearances": {},
+        "savedMessages": []
+    }
+    stats = sheet["stats"]
+    proficiencyBonus = 2 + c.level // 4
+    stats["hit-points-maximum"] = {
+        "value": c.maxHP,
+        "section": "info",
+        "type": "number",
+        "hidden": True,
+        "parent": "hit-points",
+        "local": True
+    }
+    stats["hit-points"] = {
+        "value": c.maxHP,
+        "section": "info",
+        "type": "health",
+        "local": True
+    }
+    stats["hit-points-temporary"] = {
+        "value": 0,
+        "section": "info",
+        "type": "number",
+        "local": True,
+        "hidden": True,
+        "parent": "hit-points"
+    }
+    stats["armor-class"] = {
+        "value": c.armorClass,
+        "section": "info",
+        "type": "number"
+    }
+    stats["proficiency"] = {
+        "value": proficiencyBonus,
+        "expression": "2+floor(level/4)",
+        "section": "info",
+        "type": "number"
+    }
+    stats["initiative"] = {
+        "value": initiative,
+        "section": "info",
+        "type": "number",
+        "roll": "!r initiative = 1d20 + initiative"
+    }
+    for statName in dnd_stats:
+        stat_shorthand = statName[:3]
+        stats[statName] = {
+            "expression": f"floor(({statName}-score - 10) / 2)",
+            "type": "ability",
+            "value": c.stat_modifier[statName],
+            "section": "abilities",
+            "roll": f"{statName} check: {{1d20 + {statName}}}"
+        }
+        stats[f"{statName}-score"] = {
+            "parent": statName,
+            "value": c.stats[statName],
+            "type": "number",
+            "hidden": True
+        }
+        stats[f"{statName}-save"] = {
+            "expression": f"{statName} + ({statName}-save-proficiency ? proficiency : 0)",
+            "value": 0,
+            "type": "saving-throw",
+            "section": "saving-throws",
+            "roll": f"{stat_shorthand.upper()} save: {{1d20 + {statName}-save}}"
+        }
+        stats[f"{statName}-save-proficiency"] = {
+            "parent": f"{statName}-save",
+            "value": statName in c.saves,
+            "type": "checkbox",
+            "hidden": True
+        }
+    stats["level"] = {
+        "value": c.level,
+        "section": "info",
+        "type": "number"
+    }
+
+    for skillName, relevant_stat in dnd_skills.items():
+        skillExpression = f"{relevant_stat} + ({skillName}-expertise ? proficiency*2 : {skillName}-proficiency ? proficiency : 0)"
+        stats[skillName] = {
+            "expression": skillExpression,
+            "value": 0,
+            "subtitle": relevant_stat[:3],
+            "type": "skill",
+            "section": "skills",
+            "roll": f"{skillName.capitalize()} check: {{1d20 + {skillName}}}"
+        }
+        stats[f"{skillName}-proficiency"] = {
+            "value": skillName in c.skills,
+            "type": "checkbox",
+            "hidden": True,
+            "parent": skillName
+        }
+        stats[f"{skillName}-expertise"] = {
+            "value": skillName in c.skills and c.skills[skillName] == 2,
+            "type": "checkbox",
+            "hidden": True,
+            "parent": skillName
+        }
+
+    sheet["info"] = {
+        "description": "<p><strong>Personality traits:</strong> Enter information here </p><p>&nbsp;</p>",
+        "notes": "<p><strong>Ideals:</strong> character ideals here</p><p><strong>Bonds:</strong> character bonds here</p><p><strong>Flaws:</strong> Character flaws here</p><p><strong>Backstory:</strong> Fill in backstory here&nbsp;</p>",
+        "sections": [
+            {
+                "name": "combat",
+                "position": "left",
+                "tab": 1,
+                "savedMessages": False
+            },
+            {
+                "name": "at-will",
+                "position": "left",
+                "tab": 1,
+                "savedMessages": False
+            },
+            {
+                "name": "short-rest",
+                "position": "left",
+                "tab": 1,
+                "savedMessages": False
+            },
+            {
+                "name": "once-per-day",
+                "position": "left",
+                "tab": 1,
+                "savedMessages": False
+            },
+            {
+                "name": "combat-messages",
+                "position": "bottom",
+                "tab": 1,
+                "savedMessages": True
+            },
+            {
+                "name": "About-Character-Sheet",
+                "position": "bottom",
+                "tab": 1,
+                "savedMessages": False
+            },
+            {
+                "name": "Feats",
+                "position": "bottom",
+                "tab": 2,
+                "savedMessages": False
+            },
+            {
+                "name": "saving-throws",
+                "position": "right",
+                "tab": 2
+            },
+            {
+                "name": "senses",
+                "position": "right",
+                "tab": 2
+            },
+            {
+                "name": "skills",
+                "position": "right",
+                "tab": 2
+            },
+            {
+                "name": "overview",
+                "position": "left",
+                "tab": 3,
+                "savedMessages": False
+            },
+            {
+                "name": "abilities",
+                "position": "bottom",
+                "tab": 3
+            },
+            {
+                "name": "info",
+                "position": "left",
+                "tab": 3
+            },
+            {
+                "name": "details",
+                "position": "left",
+                "tab": 3
+            },
+            {
+                "name": "char-sheet-internal",
+                "position": "bottom",
+                "tab": 3,
+                "hidden": True,
+                "__comment": "Marking a section hidden currently has no effect, but leave it in in case the next rev implements it.",
+                "savedMessages": False
+            },
+            {
+                "name": "actions",
+                "position": "bottom",
+                "tab": 4,
+                "savedMessages": True
+            },
+            {
+                "name": "spells",
+                "position": "bottom",
+                "tab": 4,
+                "savedMessages": True
+            }
+        ]
+    }
+    sheet['appearances'] = []
+
+    sheet['savedMessages'] = []
+    json.dump(sheet, open(f'{c.name.lower().replace(" ", "_")}_tableplop.json', 'w'), indent=4)
+
+    print("Character Information".center(41, '-'))
+    print("Name:", c.name)
+    print("Level:", c.level)
+    print("Class:", c.classNames)
+    print("Stats:", c.stats)
+    print("AC:", c.armorClass)
+    print("Skills:", list(c.skills.keys()))
+    print("Saves:", list(c.saves.keys()))
+    print()
+    print('Tableplop Character JSON created!')
+    print(f'To see the file, open {os.getcwd()}')
+
+
+if __name__ == "__main__":
+    main()
